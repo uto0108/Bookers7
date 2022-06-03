9ba72c0d22bfb6126762ada717c7d40b02f51b92
EC2 Default User <ec2-user@ip-172-31-1-111.ec2.internal>
2022-06-03 09:07:24 +0000

    [finish]bookers2-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,22 +12,22 @@ class User < ApplicationRecord
 
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
-    
-    # フォローしたときの処理
+
+
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-  # フォローを外すときの処理
+
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
-  # フォローしているか判定
+
   def following?(user)
     followings.include?(user)
   end
